4a26f9eeac6780c3dc5e4faea4d196733e2edee5
Joshua Siddle <106534417+Joshsiddle@users.noreply.github.com>
2022-07-11 20:45:38 +0100

    added products

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
+
+
 async function main() {
     const products = await fetch('https://fakestoreapi.com/products');
     const productsData = await products.json();
-    const productListEl = document.querySelector(".product-list");
+    const productListEL = document.querySelector(".product-list");
 
-    productListEl.innnerHTML = productsData
-    .map(
-        (product) => ` <div class="product-card">
-        <div class="product-card__container">
-            <h3>Product title</h3>
-            <p><b>Price:</b>£00.00</p>
-            <p><b>Category</b>unisex clothing</p>
-            <p><b>Rating:</b>4.5</p>
-            <p><b>Count:</b>0000</p>
+    productListEL.innerHTML = productsData.map((product) => ` <div class="product-card">
+    <div class="product-card__container">
+        <img class="product__img" src=${product.image}>
+        <div class="product__info--wrapper">
+            <h3>${product.title}</h3>
+            <p><b>£${product.price.toFixed(2)}</b></p>
+            <p><b>${product.category}</b></p>
+            <p><b>${ratingsHTML(product.rating.rate)}</b></p>
         </div>
-    </div>`
-    ).join("");
+    </div>
+</div>`).join("");
+}
+
+function ratingsHTML(rate) {
+    let ratingHTML = '';
+
+    for (let i = 0; i < Math.floor(rate); ++i) {
+        ratingHTML += '<i class="fas fa-star"></i>\n';
+    }
+
+    if (!Number.isInteger(rate)) {
+        ratingHTML += '<i class="fas fa-star-half-alt"></i>\n';
+    }
+    return ratingHTML;
+}
+
+function filterBooks(event) {
+    renderBooks(event.target.value);
 }
 
 
-
-    main();
+main();
 
